9d1381dd51b6df823afb3a37df94121a2a7171de
PospolitaNV <nikita7asics@gmail.com>
2017-12-28 18:11:46 +0300

    Update InputCheckUtil.java

--- src/notes/Utils/InputCheckUtil.java
+++ src/notes/Utils/InputCheckUtil.java
@@ -1,6 +1,7 @@
 package notes.Utils;
 
 public class InputCheckUtil {
+    //Можно заменить весь метод на StringUtils.isEmpty(string) ---- см. добавление зависимостей через Maven (ApacheCommons)
     public static boolean isInputValid(String string) {
         return string != null && string.length() > 0;
     }
